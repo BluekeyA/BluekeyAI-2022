e91bab3239b06897d4ad0e0880a54705992ad3f5
BluekeyAi_hub <peter@bluekeyai.com>
2022-02-18 22:31:21 +0000

    second commit

--- apis/config.py
+++
@@ -1,8 +0,0 @@
-"""
-SQL DATABASE DETAILS
-"""
-
-db_config = {"user": "BK_AI",
-           'password': 'BluekeyAI_2021',
-           'host': 'bluekeyai.c8q5mltlbooz.us-east-2.rds.amazonaws.com',
-           'database': "BK_AI"}
